0c05bc296f860183a178f8d436a98c5ca6d5f84c
RenanBjj <97264243+RenanBjj@users.noreply.github.com>
2023-11-29 02:10:46 +0000

    add "job itens" in app.py and add new page jobitem.html to open separated

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+
+JOBS = [
+  {
+    'id':1,
+    'title':'Analista de dados',
+    'local':'São Paulo',
+    'salario': 'R$ 3500,00'
+  },
+  {
+    'id':2,
+    'title':'cientista de dados',
+    'local':'São Paulo',
+    'salario': 'R$ 3500,00'
+  }
+]
 
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-  return "Hello  Marina"
+  return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
